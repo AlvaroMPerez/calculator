66ee9e5a033f709e0bf9c61e2d58d4c217b5ee38
lilvallo <x_vallo_x@hotmail.com>
2023-11-30 10:29:59 -0600

    no se que poner en este

--- scripts.js
+++ scripts.js
@@ -14,28 +14,33 @@ function multiply(a,b){
 };
 
 function divide(a,b){
-    if(a || b >0){
+    if(a && b >0){
         let divideVar = a / b
     }else{
-        console.log&("se introdujo dos 00")
+        console.log("se introdujo dos 00")
     }
     return divideVar
 };
 
+let display = document.getElementById("display");
 
-const ceBtn = document.getElementById("ce");
-const cBtn = document.getElementById("c");
-const addBtn = document.getElementById("add");
-const multiplyBtn = document.getElementById("multiply");
-const divideBtn = document.getElementById("divide");
-const subtractBtn = document.getElementById("substract");
-const zeroBtn = document.getElementById("0");
-const oneBtn = document.getElementById("1");
-const twoBtn = document.getElementById("2");
-const threeBtn = document.getElementById("3");
-const fourBtn = document.getElementById("4");
-const fiveBtn = document.getElementById("5");
-const sixBtn = document.getElementById("6");
-const sevenBtn = document.getElementById("7");
-const eightBtn = document.getElementById("8");
-const nineBtn = document.getElementById("9");
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(button =>{
+
+    if(button.id === "ce"){
+        display.textContent = 0;
+        return
+    }
+
+    button.addEventListener("click",()=>{
+        let pushedBtn = button.textContent;
+        if(display.textContent == 0){
+            display.textContent = pushedBtn;
+        }else{
+            display.textContent += pushedBtn;
+        }
+        
+    })
+    
+});
